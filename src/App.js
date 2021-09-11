17d18d5df6c0ef65912d6c499253711badf1ce68
venkat-vs <venkat.ui18@gmail.com>
2021-09-11 11:42:28 -0500

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 export default function App() {
+  const [count, setCount] = useState(5);
+
+  function incrementCount() {
+   setCount(prevCount => prevCount + 1) 
+  }
+
+  function decrementCount() {
+    setCount(prevCount => prevCount - 1)
+  }
   return (
     <div>
       <h1>React Hooks</h1>
+      <button onClick={incrementCount}>Increment</button>
+      <pre>
+        <span>{count}</span>
+      </pre>
+      <button onClick={decrementCount}>Decrement</button>
     </div>
   );
 }
